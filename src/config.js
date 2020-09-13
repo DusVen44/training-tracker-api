ff6abcc5748af9ce3bc9e484a5ab036a7c10bf31
Dustin Venable <Venny44@yahoo.com>
2020-09-13 13:44:05 -0500

    added production migration

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://venny@localhost/training-tracker',
-    JWT_SECRET: process.env.JWT_SECRET || 'this-is-the-secret'
+    JWT_SECRET: process.env.JWT_SECRET || 'this-is-the-secret',
   }
